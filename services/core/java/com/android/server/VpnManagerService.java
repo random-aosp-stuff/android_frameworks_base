7cad176d20d284b859d9324a130c30dfe9e6e80a
Oliver Scott <olivercscott@gmail.com>
2021-07-08 10:41:43 -0400

    Global VPN feature
    * Modify existing VPN user range functions to conditionally have traffic
      from all users pass through the global VPN.
      These functions are called when:
        1. Starting a VPN
        2. Adding a user
        3. Removing a user
    * Disallow starting VPNs in secondary users when a global VPN is set
    * Show Global VPN icon on all users
    * Only reset setting when the VPN package is removed on the main user
    
    https://review.calyxos.org/c/CalyxOS/platform_calyx-sdk/+/7540/2/packages/LineageSettingsProvider/res/values/defaults.xml#114
    sets the default value of global_vpn_app to an empty string.
    Handle it by checking TextUtils.isEmpty().
    
    Issue: calyxos#352
    Issue: calyxos#833
    Issue: calyxos#2761
    Change-Id2: I42616cc1f4e39e1dad739d81f6d5c55e218be995
    Change-Id3: Ic97d5eb1bea7f5420ef87933b1818d860d3ae429
    Change-Id: Id303036300c1d6b0efa131886d768367f22f4a29

--- services/core/java/com/android/server/VpnManagerService.java
+++ services/core/java/com/android/server/VpnManagerService.java
@@ -72,6 +72,8 @@ import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.List;
 
+import lineageos.providers.LineageSettings;
+
 /**
  * Service that tracks and manages VPNs, and backs the VpnService and VpnManager APIs.
  * @hide
@@ -891,6 +893,12 @@ public class VpnManagerService extends IVpnManager.Stub {
             }
 
             vpn.refreshPlatformVpnAppExclusionList();
+
+            if (TextUtils.equals(vpn.getPackage(), packageName) && userId == UserHandle.USER_SYSTEM
+                    && vpn.isGlobalVpn()) {
+                LineageSettings.Global.putString(mContext.getContentResolver(),
+                        LineageSettings.Global.GLOBAL_VPN_APP, "");
+            }
         }
     }
 
