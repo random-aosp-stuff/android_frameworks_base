23469c951cb5f54d3eb23c7baef5d45eece9ae33
Oliver Scott <olivercscott@gmail.com>
2024-05-06 15:32:27 -0400

    Implement internet access for new apps setting
    React as early as possible to PACKAGE_ADDED to avoid race conditions
    
    Issue: calyxos#2112
    Change-Id: I454dfbca879067f35cfd011122f5a8b76a1c7f6b

--- services/core/java/com/android/server/net/NetworkPolicyManagerService.java
+++ services/core/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -332,6 +332,8 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.function.IntConsumer;
 
+import lineageos.providers.LineageSettings;
+
 /**
  * Service that maintains low-level network policy rules, using
  * {@link NetworkStatsService} statistics to drive those rules.
@@ -1479,8 +1481,14 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
                 // Clear the cache for the app
                 synchronized (mUidRulesFirstLock) {
                     mInternetPermissionMap.delete(uid);
-                    if (!hasInternetPermissionUL(uid) && !isSystemApp(uid)) {
-                        Slog.i(TAG, "ACTION_PACKAGE_ADDED for uid=" + uid + ", no INTERNET");
+                    int userId = UserHandle.getUserId(uid);
+                    UserInfo parentUser = mUserManager.getProfileParent(userId);
+                    if ((!hasInternetPermissionUL(uid) && !isSystemApp(uid)) ||
+                            LineageSettings.Secure.getIntForUser(mContext.getContentResolver(),
+                                    LineageSettings.Secure.DEFAULT_RESTRICT_NETWORK_DATA, 0,
+                                    parentUser == null ? UserHandle.getUserId(uid) : parentUser.id)
+                                    == 1) {
+                        Slog.i(TAG, "ACTION_PACKAGE_ADDED for uid=" + uid + ", no internet");
                         addUidPolicy(uid, POLICY_REJECT_ALL);
                     }
                     updateRestrictionRulesForUidUL(uid);
