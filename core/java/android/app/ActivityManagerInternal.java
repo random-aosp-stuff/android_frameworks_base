7ea319372dbc2e7a1a5566796b81172eeb2e2092
Tommy Webb <tommy@calyxinstitute.org>
2025-05-05 20:54:42 +0000

    Restart backed-up apps by default
    Amend the "Restart apps that were stopped by backup" change to make its
    behavior the default.
    
    The new behavior will be on automatically but can be turned off via:
    `adb shell setprop persist.testing.restart_processes_after_backup false`
    
    Issue: calyxos#2816
    Change-Id: I1ef7913b3db92a84fabed0828ef92af97f0beec3

--- core/java/android/app/ActivityManagerInternal.java
+++ core/java/android/app/ActivityManagerInternal.java
@@ -1381,6 +1381,6 @@ public abstract class ActivityManagerInternal {
 
     public static boolean enableRestartProcessesAfterBackup() {
         return SystemProperties.getBoolean("persist.testing.restart_processes_after_backup",
-                false);
+                true);
     }
 }
